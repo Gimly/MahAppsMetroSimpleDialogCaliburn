8cbe094c365ab95e422ac55921d92d495448705a
Xavier Hahn <xavier.hahn@gmail.com>
2014-10-27 12:57:29 +0100

    Added example with WindowManager to show that it is a SimpleDialog issue

--- MahAppsMetroCaliburnTest/ShellViewModel.cs
+++ MahAppsMetroCaliburnTest/ShellViewModel.cs
@@ -10,15 +10,27 @@ namespace MahAppsMetroCaliburnTest
     public class ShellViewModel : Screen, IShell
     {
         private IDialogManager dialogManager;
+        private IWindowManager windowManager;
 
-        public ShellViewModel(IDialogManager dialogManager)
+        public ShellViewModel(IDialogManager dialogManager, IWindowManager windowManager)
         {
             this.dialogManager = dialogManager;
+            this.windowManager = windowManager;
         }
 
         public async void ShowMetroDialog()
         {
             var result = await this.dialogManager.ShowDialog(new TestMahAppsDialogViewModel());
+        }
+
+        public void ShowPopupWithSimpleDialog()
+        {
+            this.windowManager.ShowDialog(new TestMahAppsDialogViewModel());
+        }
+
+        public void ShowPopupUserControl()
+        {
+            this.windowManager.ShowDialog(new UserControlDialogViewModel());
         }
     }
 }
---
+++ MahAppsMetroCaliburnTest/ViewModels/Dialogs/UserControlDialogViewModel.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MahAppsMetroCaliburnTest.ViewModels.Dialogs
+{
+    public class UserControlDialogViewModel : DialogViewModel<string>
+    {
+        private string firstName;
+        public string FirstName
+        {
+            get
+            {
+                return this.firstName;
+            }
+            set
+            {
+                this.firstName = value;
+                this.NotifyOfPropertyChange(() => FirstName);
+            }
+        }
+
+        private string lastName;
+        public string LastName
+        {
+            get
+            {
+                return this.lastName;
+            }
+            set
+            {
+                this.lastName = value;
+                this.NotifyOfPropertyChange(() => LastName);
+            }
+        }
+
+        private string helloMessage;
+        public string HelloMessage
+        {
+            get
+            {
+                return this.helloMessage;
+            }
+            set
+            {
+                this.helloMessage = value;
+                this.NotifyOfPropertyChange(() => HelloMessage);
+            }
+        }
+
+        public void SayHello()
+        {
+            this.HelloMessage = String.Format("Hello, {0} {1}", firstName, lastName);
+        }
+    }
+}
